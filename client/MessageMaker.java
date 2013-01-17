5546827e10dd8f3273770e20a41486d6c58cc282
Jason Tierney <jason.tierney@live.com>
2013-01-17 17:11:42 -0500

    Revert "commit"
    This reverts commit 95c5c640f4f567b571daf888f1245bcc7f5ef054.

--- client/MessageMaker.java
+++ client/MessageMaker.java
@@ -14,18 +14,83 @@ import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+/*
+<?xml version="1.0" encoding="UTF-8"?>
+<MESSAGE>
+  <FROM>user uuid</FROM>
+  <TYPE> text or message </TYPE>
+  <TO> user or all or group </TO>
+  <ACTION> block nameChange </ACTION>
+  <TEXT>message text</TEXT>
+  <ID>message uuid</ID>
+</MESSAGE>
+*/
 
-
+class MessageType {
+    public final static String text = "text";
+    public final static String action = "action";
+}
 public class MessageMaker {
+    
 
-    public String makeMessage(ChatClient from, String to, ServerAction action,
-			      String text) {
-	return "";
+    public static String textMessage(ChatClient from, String to, String text) {
+	DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder docBuilder = null;
+	try {
+	    docBuilder = docFactory.newDocumentBuilder();
+	} catch(ParserConfigurationException pce) {
+	    pce.printStackTrace();
+	}
+	
+	// root elements
+	Document doc = docBuilder.newDocument();
+	Element msg = doc.createElement("MSG");
+	doc.appendChild(rootElement);
+	
+	// staff elements
+	Element type = doc.createElement("TYPE");
+	type.appendChild(doc.createTextNode("TXT"));
+	rootElement.appendChild(type);
+	
+	// set attribute to staff element
+	//Attr attr = doc.createAttribute("id");
+	//attr.setValue("1");
+	//staff.setAttributeNode(attr);
+	
+	// shorten way
+	// staff.setAttribute("id", "1");
+	
+	// firstname elements
+	Element to = doc.createElement("TO");
+	firstname.appendChild(doc.createTextNode("someuser"));
+	msg.appendChild(firstname);
+	
+	// lastname elements
+	Element to = doc.createElement("lastname");
+	lastname.appendChild(doc.createTextNode("mook kim"));
+	msg.appendChild(lastname);
+	
+	// nickname elements
+	Element nickname = doc.createElement("nickname");
+	nickname.appendChild(doc.createTextNode("mkyong"));
+	type.appendChild(nickname);
+	
+	// salary elements
+	Element salary = doc.createElement("salary");
+	salary.appendChild(doc.createTextNode("100000"));
+	type.appendChild(salary);
+	try {
+	    TransformerFactory tf = TransformerFactory.newInstance();
+	    Transformer transformer = tf.newTransformer();
+	    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+	    StringWriter writer = new StringWriter();
+	    transformer.transform(new DOMSource(doc), new StreamResult(writer));
+	    String output = writer.getBuffer().toString().replaceAll("\n|\r", "");
+	    return output;
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+	return "asf";
     }
     
-    public static String makeNameChange(ChatClient from, String to, String newName) {
-
-    }
-					
-g    
 }
--- shared/ServerAction.java
+++
@@ -1,3 +0,0 @@
-public enum ServerAction {
-    block, nameChange, groupJoin, none;
-}
