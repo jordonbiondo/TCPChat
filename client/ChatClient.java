e22132c7d0d39569f6fc74d8407d0f32bafc8a6c
Jordon Biondo <biondoj@mail.gvsu.edu>
2013-01-17 16:23:07 -0500

    Client folder

---
+++ client/ChatClient.java
@@ -0,0 +1,8 @@
+public class ChatClient {
+    
+    public static void main(String[] args) {
+	ChatClient c = new ChatClient();
+	
+	System.out.println(MessageMaker.textMessage(c, "sdf", "fds"));
+    }
+}
