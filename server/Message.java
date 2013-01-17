9472424d69eba6dcb6bf540d92d441da32f615b1
Jason Tierney <jason.tierney@live.com>
2013-01-17 17:22:59 -0500

    Modified Message and MessageParser
    Updated Message to have a default empty constructor.
    Updated MessageParser to parse messages.

--- server/Message.java
+++ server/Message.java
@@ -1,16 +1,3 @@
-
-
-/*
-<?xml version="1.0" encoding="UTF-8"?>
-<MESSAGE>
-  <FROM>username</FROM>
-  <TO> username or  * or group name </TO>
-  <ACTION> block or nameChange or none </ACTION>
-  <TEXT>message text</TEXT>
-  <ID>message uuid</ID>
-</MESSAGE>
-*/
-
 /*
   from-uuid:username or not:action:message-uuid:text
 */
@@ -19,14 +6,14 @@
 
 public class Message {
 
-    public final UUID sender;
-    public final String receiver;
-    public final String text;
-    public final ServerAction action;
-    public final UUID messageID;
+    public UUID sender;
+    public String receiver;
+    public String text;
+    public ServerAction action;
+    public UUID messageID;
 
-	public Message(UUID sender, String receiver, UUID messageID, 
-		       ServerAction action, String text) {
+	public Message()
+	{
 	
     }
 }
