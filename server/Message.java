976c248616b26d07ab5d19c941056a0e9d0dd898
Jordon Biondo <biondoj@mail.gvsu.edu>
2013-01-17 16:44:06 -0500

    message info

--- server/Message.java
+++ server/Message.java
@@ -3,13 +3,16 @@
 /*
 <?xml version="1.0" encoding="UTF-8"?>
 <MESSAGE>
-  <FROM>user uuid</FROM>
-  <TYPE> text or message </TYPE>
-  <TO> user or all or group </TO>
-  <ACTION> block nameChange </ACTION>
+  <FROM>username</FROM>
+  <TO> username or  * or group name </TO>
+  <ACTION> block or nameChange or none </ACTION>
   <TEXT>message text</TEXT>
   <ID>message uuid</ID>
 </MESSAGE>
+*/
+
+/*
+  from-uuid:username or not:action:message-uuid:text
 */
 
 
@@ -20,7 +23,10 @@ public class Message {
     public final String receiver;
     public final String text;
     public final ServerAction action;
+    public final UUID messageID;
 
-    public Message(UUID sender, String receiver, String text, ServerAction action) {
+	public Message(UUID sender, String receiver, UUID messageID, 
+		       ServerAction action, String text) {
+	
     }
 }
